b204ed47d0bba57e2761110a2709b574e53593e3
Daniel Hladek <daniel.hladek@tuke.sk>
2022-11-08 13:55:35 +0100

    v0.3

--- scoreboard/scoreboard/settings.py
+++ scoreboard/scoreboard/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # https://django-ckeditor.readthedocs.io/en/latest/
-    "ckeditor"
+    "ckeditor",
 ]
 
 #CKEDITOR_CONFIGS = {
