d9fc41e83e9d0586cf151b4b56941d483c899e09
Daniel Hladek <daniel.hladek@tuke.sk>
2022-11-22 10:54:56 +0100

    run in frame, board templates

--- scoreboard/contest/urls.py
+++ scoreboard/contest/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('team/<int:team_id>', views.contest_team, name='contest_team'),
     path('competition_board/<int:competition_id>', views.competition_board, name='competition_board'),
     path('frame/board/<int:competition_id>', views.frame_board, name='frame_board'),
+    path('frame/runs/<int:competition_id>', views.frame_runs, name='frame_runs'),
+    path('frame/participants/<int:contest_id>', views.frame_participants, name='frame_participants'),
+    path('frame/competitions/<int:contest_id>', views.frame_competitions, name='frame_competitions'),
 ]
--- scoreboard/contest/views.py
+++ scoreboard/contest/views.py
@@ -5,16 +5,19 @@ from django.shortcuts import render
 from django.db.models import Max
 from .models import Competition,Contest,Run,Team,ItemStates
 
-from django.views.decorators.clickjacking import xframe_options_exempt
 
 def get_competition_results(competition):
     return competition.participants.all().annotate(max_score=Max("run__score")).filter(run__competition_id=competition.id)
 
-@xframe_options_exempt
 def frame_board(request,competition_id):
     competition = Competition.objects.get(pk=competition_id)
     parti = get_competition_results(competition)
-    return render(request, 'contest/frame_board.html', {"results":parti})
+    return render(request, 'contest/frame_board.html', {"competition":competition,"results":parti})
+
+def frame_runs(request,competition_id):
+    competition = Competition.objects.get(pk=competition_id)
+    runs = Run.objects.filter(competition_id=competition.id).order_by("-score","team__name","start_time")
+    return render(request, 'contest/frame_runs.html', {"runs":runs})
 
 # ...
 def competition_board(request, competition_id):
@@ -22,7 +25,7 @@ def competition_board(request, competition_id):
         # TODO add timer
         competition = Competition.objects.get(pk=competition_id)
         participants = competition.participants.all().order_by("name")
-        parti = get_competition_results(competition_id)
+        parti = get_competition_results(competition)
         runs = Run.objects.filter(competition_id=competition.id).order_by("-score","team__name","start_time")
     except Competition.DoesNotExist:
         raise Http404("Competition does not exist")
@@ -34,6 +37,16 @@ def contest_competitions(request,contest_id):
     competitions = contest.competition_set.select_related()
     teams = contest.team_set.select_related().all()
     return render(request,"contest/competitions.html",{"contest":contest,"teams":teams,"competitions":competitions})
+
+def frame_competitions(request,contest_id):
+    contest = Contest.objects.get(pk=contest_id)
+    competitions = contest.competition_set.select_related()
+    return render(request,"contest/frame_competitions.html",{"competitions":competitions})
+
+def frame_participants(request,contest_id):
+    contest = Contest.objects.get(pk=contest_id)
+    teams = contest.team_set.select_related().all()
+    return render(request,"contest/frame_participants.html",{"teams":teams})
 
 def contest_team(request,team_id):
     team = Team.objects.get(pk=team_id)
